47d9dbde28a1b2f573d12d76e1723e0333ec3518
Maycon Vieira <mayconguill@gmail.com>
2025-05-26 15:43:25 -0300

    Defensive the chess layer and little improvement in board printing

--- src/model/chesslayer/entitie/ChessMatch.java
+++ src/model/chesslayer/entitie/ChessMatch.java
@@ -1,7 +1,6 @@
 package model.chesslayer.entitie;
 
 import model.boardlayer.entitie.Board;
-import model.boardlayer.entitie.Position;
 import model.chesslayer.enums.Color;
 import model.chesslayer.pieces.King;
 import model.chesslayer.pieces.Rook;
@@ -23,10 +22,24 @@ public class ChessMatch {
         }
         return chessPieces;
     }
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
 
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 
 }
