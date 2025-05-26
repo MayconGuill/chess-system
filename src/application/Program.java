3b744c798fe5fe69fea62314ae5089868948ebe8
Maycon Vieira <mayconguill@gmail.com>
2025-05-26 16:20:19 -0300

    Handling exception and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,38 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import model.chesslayer.entitie.ChessMatch;
 import model.chesslayer.entitie.ChessPiece;
 import model.chesslayer.entitie.ChessPosition;
+import model.chesslayer.exception.ChessException;
 
 public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         ChessMatch chessMatch = new ChessMatch();
-        while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(scanner);
-            
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(scanner);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        while (true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(scanner);
+                
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(scanner);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException exception) {
+                System.out.print(exception.getMessage());
+                scanner.nextLine();
+            } catch (InputMismatchException exception) {
+                System.out.println(exception.getMessage());
+                scanner.nextLine();
+            }
         }
     }
 }
